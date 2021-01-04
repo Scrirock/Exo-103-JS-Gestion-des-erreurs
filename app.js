f9533013ea99e2ad1ff6918e5241cbd3d100ba28
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-04 14:09:51 +0100

    hop

---
+++ app.js
@@ -0,0 +1,8 @@
+try {
+    document.getElementById("jAiOublieZut").innerHTML = "Je change mon div avec l'id le plus simple";
+}
+
+catch (e){
+    alert("L'element " + e.element + "n'existe pas");
+}
+
